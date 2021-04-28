70c51c39ff3f7c7091a5c2039d711bb52e394c29
m-yahya <yahya_gis@hotmail.com>
2021-04-28 16:12:29 +0200

    Update code for part family tree

--- js/main.js
+++ js/main.js
@@ -3,6 +3,72 @@ const data = {
     img: "father.png",
     name: "Jan Doe",
     age: "50",
+    children: [
+      {
+        child: {
+          img: "child_1.png",
+          name: "child 1",
+          age: "25",
+        },
+      },
+      {
+        child: {
+          img: "child_2.png",
+          name: "child 2",
+          age: "22",
+          children: [
+            {
+              grandChild: {
+                img: "child_3.png",
+                name: "grand child 1",
+                age: "12",
+              },
+            },
+          ],
+        },
+      },
+      {
+        child: {
+          img: "child_4.png",
+          name: "child 3",
+          age: "16",
+          children: [
+            {
+              grandChild: {
+                img: "child_5.png",
+                name: "grand child 1",
+                age: "18",
+                children: [
+                  {
+                    grandgrandChild: {
+                      img: "child_6.png",
+                      name: "grand grand child 1",
+                      age: "13",
+                    },
+                  },
+                  {
+                    grandgrandChild: {
+                      img: "child_7.png",
+                      name: "grand grand child 1",
+                      age: "10",
+                      children: [
+                        {
+                          grandgrandChild: {
+                            img: "child_8.png",
+                            name: "grand grand grand child 1",
+                            age: "08",
+                          },
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
 };
 
@@ -35,11 +101,17 @@ const getDetails = (details) => {
   // iterate over the detail items of object
   for (const detail in details) {
     // fetch the value of each item
-
     if (detail == "img") {
       markupArray.push(
         `<img src="./img/${details[detail]}" alt="${details[detail]}">`
       );
+    } else if (detail == "children") {
+      markupArray.push("<ul>");
+      details[detail].forEach((element) => {
+        getItems(element);
+      });
+
+      markupArray.push("</ul>");
     } else {
       markupArray.push(`<span> ${details[detail]} </span>`);
     }
@@ -50,6 +122,5 @@ const getDetails = (details) => {
 window.onload = () => {
   createList(data);
   markupArray.push("</ul>");
-  console.log(markupArray);
   $("#list").html(markupArray.join(""));
 };
